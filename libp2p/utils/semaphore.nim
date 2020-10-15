c2826c6a5f8d8fc1a0ff03c4c06e4c891fbf8ca2
Dmitriy Ryajov <dryajov@gmail.com>
2020-10-15 16:47:37 -0600

    cleanup semaphore

--- libp2p/utils/semaphore.nim
+++ libp2p/utils/semaphore.nim
@@ -7,19 +7,32 @@
 ## This file may not be copied, modified, or distributed except according to
 ## those terms.
 
-import deques
+import sequtils
 import chronos, chronicles
 
 # TODO: this should probably go in chronos
+
+logScope:
+  topics = "semaphore"
 
 type
   AsyncSemaphore* = ref object of RootObj
     size*: int
     count*: int
-    queue: Deque[Future[void]]
+    queue: seq[Future[void]]
 
 proc init*(T: type AsyncSemaphore, size: int): T =
   T(size: size, count: size)
+
+proc tryAcquire*(s: AsyncSemaphore): bool =
+  ## Attempts to acquire a resource, if successful
+  ## returns true, otherwise false
+  ##
+
+  if s.count > 0 and s.queue.len == 0:
+    s.count.dec
+    trace "Acquired slot", available = s.count, queue = s.queue.len
+    return true
 
 proc acquire*(s: AsyncSemaphore): Future[void] =
   ## Acquire a resource and decrement the resource
@@ -28,34 +41,15 @@ proc acquire*(s: AsyncSemaphore): Future[void] =
   ## the resource count goes above 0 again.
   ##
 
-  var fut = newFuture[void]("AsyncSemaphore.acquire")
-  fut.cancelCallback = proc(udata: pointer) =
-    ## if future got canceled, increment the
-    ## resource counter
-    ##
-    s.count.inc
-    trace "Canceling and releasing semaphore slot", available = s.count,
-                                                    queue = s.queue.len
-
-  if s.count > 0:
+  let fut = newFuture[void]("AsyncSemaphore.acquire")
+  if s.tryAcquire():
     fut.complete()
-  else:
-    s.queue.addLast(fut)
+    return fut
 
+  s.queue.add(fut)
   s.count.dec
-  trace "Acquired semaphore slot", available = s.count,
-                                   queue = s.queue.len
+  trace "Queued slot", available = s.count, queue = s.queue.len
   return fut
-
-proc tryAcquire*(s: AsyncSemaphore): bool =
-  ## Attempts to acquire a resource, if successful
-  ## returns true, otherwise false
-  ##
-
-  # acquire() will return finished
-  # futures if the resource count
-  # is less than `size`
-  return (s.acquire().finished)
 
 proc release*(s: AsyncSemaphore) =
   ## Release a resource from the semaphore,
@@ -64,33 +58,27 @@ proc release*(s: AsyncSemaphore) =
   ## internal resource count
   ##
 
-  if s.count >= s.size:
-    return
+  doAssert(s.count <= s.size)
 
-  while true:
+  if s.count < s.size:
+    trace "Releasing slot", available = s.count,
+                            queue = s.queue.len
+
     if s.queue.len > 0:
-      var fut = s.queue.popFirst()
-      trace "Releasing semaphore slot", available = s.count,
-                                        queue = s.queue.len
-
-      # skip `canceled`, since the resource
-      # count has been already adjusted in
-      # the cancellation callback
-      if fut.cancelled():
-        trace "Canceled semaphore slot, skipping", available = s.count,
-                                                   queue = s.queue.len
-        continue
-
+      var fut = s.queue.pop()
       if not fut.finished():
         fut.complete()
 
-    s.count.inc # increment the result count
-    trace "Released semaphore slot", available = s.count,
-                                     queue = s.queue.len
+    s.count.inc # increment the resource count
+    trace "Released slot", available = s.count,
+                           queue = s.queue.len
+                          #  stack = getStackTrace()
     return
 
 when isMainModule:
-  import unittest
+  import unittest, random
+
+  randomize()
 
   suite "AsyncSemaphore":
     test "should acquire":
@@ -134,24 +122,8 @@ when isMainModule:
         sema.release()
         check sema.count == 1
 
+        await sleepAsync(10.millis)
         check fut.finished()
-
-      waitFor(test())
-
-    test "should handle canceled":
-      proc test() {.async.} =
-        let sema = AsyncSemaphore.init(1)
-
-        await sema.acquire()
-        let fut = sema.acquire()
-
-        check sema.count == -1
-        check sema.queue.len == 1
-        fut.cancel()
-        sema.release()
-        check sema.count == 1
-
-        check fut.cancelled()
 
       waitFor(test())
 
@@ -189,8 +161,38 @@ when isMainModule:
         check sema.queue.len == 1
 
         sema.release()
+        sema.release()
+        sema.release()
+        sema.release()
+        sema.release()
+
         check fut.finished == true
-        check sema.count == 0
+        check sema.count == 4
         check sema.queue.len == 0
+
+      waitFor(test())
+
+    test "should restrict resource access":
+      proc test() {.async.} =
+        let sema = AsyncSemaphore.init(3)
+        var resource = 0
+
+        proc task() {.async.} =
+          try:
+            await sema.acquire()
+            resource.inc()
+            check resource > 0 and resource <= 3
+            let sleep = rand(0..10).millis
+            # echo sleep
+            await sleepAsync(sleep)
+          finally:
+            resource.dec()
+            sema.release()
+
+        var tasks: seq[Future[void]]
+        for i in 0..<10:
+          tasks.add(task())
+
+        await allFutures(tasks)
 
       waitFor(test())
